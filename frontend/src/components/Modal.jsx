dbf5d2c13960f89139ad995d1c55543346001c1c
GeorgeHolm <114369084+GeorgeHolm@users.noreply.github.com>
2024-06-26 16:58:16 -0700

    modal pops up

---
+++ frontend/src/components/Modal.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
+import "./Modal.css";
+import { useState, useEffect } from "react";
+
+export function Modal(props) {
+
+
+
+  return (
+    <div className="overlay">
+      <div className="modal" >
+
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
